1151baad18d5139962b632576eeab530a4ec4aaa
JIMIN KIM <102471138+Kareeenn@users.noreply.github.com>
2024-07-23 12:55:55 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240723/두 사람/two-person.py
@@ -0,0 +1,14 @@
+a = input()
+a = a.split()
+age1 = int(a[0])
+gen1 = a[1]
+
+b = input()
+b = b.split()
+age2 = int(b[0])
+gen2 = b[1]
+
+if (age1 >=19 and gen1 = 'M') or (age2 >=19 and gen2 = 'M'):
+    print('1')
+else :
+    print('0')
