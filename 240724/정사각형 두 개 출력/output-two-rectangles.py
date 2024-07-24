6cda748b2475c54efdb708c77e5912f090edd92c
JIMIN KIM <102471138+Kareeenn@users.noreply.github.com>
2024-07-24 15:14:28 +0900

    Title: 정사각형 두 개 출력, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/정사각형 두 개 출력/output-two-rectangles.py
+++ 240724/정사각형 두 개 출력/output-two-rectangles.py
@@ -1,8 +1,8 @@
 n = int(input())
 
 for i in range(0,2):
-    for j in range(0,2):
-        for k in range(0,2):
+    for j in range(0,n):
+        for k in range(0,n):
             print("*",end="")
         print()
     print()
