8a990e5cfe83194507ffe59d9502f2f9d7ea70b5
JIMIN KIM <102471138+Kareeenn@users.noreply.github.com>
2024-07-27 19:36:44 +0900

    Title: 별표 출력하기 8, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/별표 출력하기 8/print-star-8.py
+++ 240727/별표 출력하기 8/print-star-8.py
@@ -2,8 +2,8 @@ n = int(input())
 
 for i in range(1,n+1):
     if i%2 != 0:
-        print("* ")
+        print("* ", end="")
     else:
         for k in range(i):
-            print("* ")
+            print("* ", end="")
     print()
