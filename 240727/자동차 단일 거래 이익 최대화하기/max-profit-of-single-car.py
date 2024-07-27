3d981398a05be2828d84ac7f21830877bcfafb41
JIMIN KIM <102471138+Kareeenn@users.noreply.github.com>
2024-07-27 19:29:15 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240727/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240727/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -4,9 +4,10 @@ arr = list(map(int, input().split()))
 #k = max(arr)- min(arr)
 #팔기(i) - 사기(j)
 #사기가 더 먼저
-if n >1:
-    k = arr[1]-arr[0]
-#print(len(arr))
+if n <= 1:
+    print(0)
+else:
+    k = arr[1] - arr[0]
 
 for i in range(1,n): #팔기
     #print(i)
