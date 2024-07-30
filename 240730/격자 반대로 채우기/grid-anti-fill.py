ec97720be88923ea2e3af54d0913ba599cd2cbf9
JIMIN KIM <102471138+Kareeenn@users.noreply.github.com>
2024-07-30 13:26:40 +0900

    Title: 격자 반대로 채우기, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/격자 반대로 채우기/grid-anti-fill.py
+++ 240730/격자 반대로 채우기/grid-anti-fill.py
@@ -7,17 +7,33 @@ arr = [
 
 num = 1
 
-for j in range(n-1, -1, -1): #열
-    if j%2 == 0: 
-        # 정방향
-        for i in range(n):
-            arr[i][j]= num
-            num += 1
-    else:
-        #역방향
-        for i in range(n-1, -1, -1): #행
-            arr[i][j] = num
-            num += 1
+#n = 짝수
+if n % 2 == 0:
+    for j in range(n-1, -1, -1): #열
+    # n = 홀수면, 짝수가 역방향 / n = 짝수면, 짝수가 정방향
+        if j%2 == 0: 
+            # 정방향
+            for i in range(n):
+                arr[i][j]= num
+                num += 1
+        else:
+            #역방향
+            for i in range(n-1, -1, -1): #행
+                arr[i][j] = num
+                num += 1
+else:
+    for j in range(n-1, -1, -1): #열
+# n = 홀수면, 짝수가 역방향 / n = 짝수면, 짝수가 정방향
+        if j%2 != 0: 
+            # 정방향
+            for i in range(n):
+                arr[i][j]= num
+                num += 1
+        else:
+            #역방향
+            for i in range(n-1, -1, -1): #행
+                arr[i][j] = num
+                num += 1
 
 for i in range(n):
     for j in range(n):
