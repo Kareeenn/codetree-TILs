4f24b67bc8a26d98754f2b81757ef664511c7418
JIMIN KIM <102471138+Kareeenn@users.noreply.github.com>
2024-07-31 12:07:33 +0900

    Title: 격자 모양 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240731/격자 모양 출력하기/print-grid-shape.py
@@ -0,0 +1,21 @@
+k = input().split()
+n, m = int(k[0]), int(k[1])
+arr = []
+
+for _ in range(m):
+    arr.append(tuple(map(int, input().split())))
+
+arr2 = [
+    [0 for _ in range(m)]
+    for _ in range(m)
+]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if (i, j) in arr: #헷가림
+            arr2[i-1][j-1] = i*j
+
+for i in range(n):
+    for j in range(n):
+        print(arr2[i][j], end =" ")
+    print()
