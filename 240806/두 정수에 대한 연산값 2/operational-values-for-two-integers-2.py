b6c144008907733fd2f05788904e8c9e2a51b1e4
JIMIN KIM <102471138+Kareeenn@users.noreply.github.com>
2024-08-06 15:12:42 +0900

    Title: 두 정수에 대한 연산값 2, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 240806/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -1,7 +1,10 @@
 a,b = tuple(map(int, input().split()))
 
 def king(a,b):
-    a, b = min(a,b) +10 , max(a,b)*2
+    if a <b :
+        a, b = min(a,b) +10 , max(a,b)*2
+    else:
+        b, a = min(a,b) +10 , max(a,b)*2
     return a,b
 
 n, m = king(a,b)
